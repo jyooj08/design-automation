44c82d9f90aa896a58e69dc37eb69a04359bb374
yujijang <yujijang@gmarket.com>
2023-06-27 23:00:54 +0900

    시작화면 title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,22 @@
-/* eslint-disable @next/next/no-img-element */
 import Header from "@/components/Header";
 import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="home">
+    <>
       <Header />
-      <Link href="/select" className="btn">
-        시작하기
-      </Link>
-    </div>
+      <div className="home">
+        <div className="title mt-24 flex flex-col items-center">
+          <div className="subtitle text-[9px]">find your own building</div>
+          <div className="maintitle text-[36px]">
+            나의 <span className="font-bold">건물</span> 찾기
+          </div>
+        </div>
+        <Link href="/select" className="btn">
+          시작하기
+        </Link>
+      </div>
+    </>
   );
 };
 export default Home;
